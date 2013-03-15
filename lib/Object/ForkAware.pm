f661aab324d6b9add31a333ba35dbc92ea2c307a
Karen Etheridge <ether@cpan.org>
2013-03-15 14:58:06 -0700

    initial commit, via [Git::Init]

---
+++ lib/Object/ForkAware.pm
@@ -0,0 +1,44 @@
+use strict;
+use warnings;
+package Object::ForkAware;
+# ABSTRACT: ...
+
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+...
+
+=head1 DESCRIPTION
+
+
+=head1 FUNCTIONS/METHODS
+
+=over
+
+=item * C<foo>
+
+=back
+
+...
+
+=head1 SUPPORT
+
+Bugs may be submitted through L<https://rt.cpan.org/Public/Dist/Display.html?Name=Object-ForkAware>
+or L<bug-Object-ForkAware@rt.cpan.org>.
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+...
+
+=cut
